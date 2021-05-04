6445e9e8cf39c733d5af9f02affec041dbc5831b
Tristan <tantris.kantner@gmail.com>
2021-05-04 17:48:40 +0200

    Update index & README

--- README.md
+++ README.md
@@ -5,5 +5,3 @@
 1.When somebody enters a specific command in the command line, a block of approximately 31x9 # characters and of a random color(hex) will be generated.
 
 2.It should be able to not only accept the command itself, but also the request for a hue, luminosity and then generate random colors that match those choices.
-
-3.If a user types "ask" instead of a color name, print questions to ask the user for the name of the color and the luminosity.
--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 const randomColor = require('randomcolor');
 const chalk = require('chalk');
-const convert = require('color-convert');
 
-if (process.argv[2] === undefined) {
-  const colour = randomColor();
-  console.log(
-    chalk.hex(colour)(
-      `##############################
+let colour = randomColor();
+const str = `##############################
 ##############################
 ##############################
 #####                    #####
@@ -14,46 +10,26 @@ if (process.argv[2] === undefined) {
 #####                    #####
 ##############################
 ##############################
-##############################`,
-    ),
-  );
+##############################`;
+
+if (process.argv[2] === undefined) {
+  console.log(chalk.hex(colour)(str));
 } else if (process.argv[3] === 'light') {
-  const colour = convert.keyword.hex(process.argv[2]);
-  console.log(
-    chalk.bold.hex(colour)(`##############################
-##############################
-##############################
-#####                    #####
-#####      #${colour}       #####
-#####                    #####
-##############################
-##############################
-##############################`),
-  );
+  colour = randomColor({
+    luminosity: 'light',
+    hue: process.argv[2],
+  });
+  console.log(chalk.hex(colour)(str));
 } else if (process.argv[3] === 'dark') {
-  const colour = convert.keyword.hex(process.argv[2]);
-  console.log(
-    chalk.dim.hex(colour)(`##############################
-##############################
-##############################
-#####                    #####
-#####      #${colour}       #####
-#####                    #####
-##############################
-##############################
-##############################`),
-  );
+  colour = randomColor({
+    luminosity: 'dark',
+    hue: process.argv[2],
+  });
+  console.log(chalk.hex(colour)(str));
 } else if (chalk.keyword(process.argv[2])) {
-  const colour = convert.keyword.hex(process.argv[2]);
-  console.log(
-    chalk.hex(colour)(`##############################
-##############################
-##############################
-#####                    #####
-#####      #${colour}       #####
-#####                    #####
-##############################
-##############################
-##############################`),
-  );
+  colour = randomColor({
+    luminosity: 'random',
+    hue: process.argv[2],
+  });
+  console.log(chalk.hex(colour)(str));
 }
