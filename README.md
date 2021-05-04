da0eed3329f71937cd9da5f4f6b11aa9c97b3c2d
Tristan <tantris.kantner@gmail.com>
2021-05-04 17:18:57 +0200

    Add Index.js and dependencies

--- README.md
+++ README.md
@@ -6,6 +6,4 @@
 
 2.It should be able to not only accept the command itself, but also the request for a hue, luminosity and then generate random colors that match those choices.
 
-3.If a user types "ask" instead of a color name, print questions to ask the user for the name of the color and the luminosity. 
-
-4. If a user types a size in the format of WWxHH (eg. 31x9) before the color and luminosity, it will use this as the size of the box.
+3.If a user types "ask" instead of a color name, print questions to ask the user for the name of the color and the luminosity.
--- index.js
+++ index.js
@@ -1,1 +1,59 @@
-let color = require('color');
+const randomColor = require('randomcolor');
+const chalk = require('chalk');
+const convert = require('color-convert');
+
+if (process.argv[2] === undefined) {
+  const colour = randomColor();
+  console.log(
+    chalk.hex(colour)(
+      `##############################
+##############################
+##############################
+#####                    #####
+#####      ${colour}       #####
+#####                    #####
+##############################
+##############################
+##############################`,
+    ),
+  );
+} else if (process.argv[3] === 'light') {
+  const colour = convert.keyword.hex(process.argv[2]);
+  console.log(
+    chalk.bold.hex(colour)(`##############################
+##############################
+##############################
+#####                    #####
+#####      #${colour}       #####
+#####                    #####
+##############################
+##############################
+##############################`),
+  );
+} else if (process.argv[3] === 'dark') {
+  const colour = convert.keyword.hex(process.argv[2]);
+  console.log(
+    chalk.dim.hex(colour)(`##############################
+##############################
+##############################
+#####                    #####
+#####      #${colour}       #####
+#####                    #####
+##############################
+##############################
+##############################`),
+  );
+} else if (chalk.keyword(process.argv[2])) {
+  const colour = convert.keyword.hex(process.argv[2]);
+  console.log(
+    chalk.hex(colour)(`##############################
+##############################
+##############################
+#####                    #####
+#####      #${colour}       #####
+#####                    #####
+##############################
+##############################
+##############################`),
+  );
+}
