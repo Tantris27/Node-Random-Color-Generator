d2fed2aa99fdd76e91ce1573745c19629dd906b6
Tristan <tantris.kantner@gmail.com>
2021-05-06 17:22:48 +0200

    Update of Index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const randomColor = require('randomcolor');
 const chalk = require('chalk');
 
-let colour = randomColor();
+const colour = randomColor({
+  luminosity: process.argv[3],
+  hue: process.argv[2],
+});
 const str = `##############################
 ##############################
 ##############################
@@ -12,24 +15,4 @@ const str = `##############################
 ##############################
 ##############################`;
 
-if (!process.argv[2]) {
-  console.log(chalk.hex(colour)(str));
-} else if (process.argv[3] === 'light') {
-  colour = randomColor({
-    luminosity: 'light',
-    hue: process.argv[2],
-  });
-  console.log(chalk.hex(colour)(str));
-} else if (process.argv[3] === 'dark') {
-  colour = randomColor({
-    luminosity: 'dark',
-    hue: process.argv[2],
-  });
-  console.log(chalk.hex(colour)(str));
-} else if (process.argv[2]) {
-  colour = randomColor({
-    luminosity: 'random',
-    hue: process.argv[2],
-  });
-  console.log(chalk.hex(colour)(str));
-}
+console.log(chalk.hex(colour)(str));
